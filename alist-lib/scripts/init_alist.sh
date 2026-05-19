9a59245e11948043fa6f41f59da8f610a3e8edcf
kimfatman <237029918@QQ.com>
2026-05-19 16:38:33 +0800

    Fix init_alist.sh: use main branch instead of latest tag to avoid 115 driver dependency issue

--- alist-lib/scripts/init_alist.sh
+++ alist-lib/scripts/init_alist.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 GIT_REPO="https://github.com/AlistGo/alist.git"
 echo "Using AList repo: $GIT_REPO"
-TAG_NAME=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --sort='version:refname' --tags $GIT_REPO | tail --lines=1 | cut --delimiter='/' --fields=3)
-echo "AList - ${TAG_NAME}"
+
+# Use main branch instead of latest tag to avoid dependency issues
+BRANCH="main"
+echo "Using branch: $BRANCH"
+
 rm -rf ./src
 unset GIT_WORK_TREE
-git clone --branch "$TAG_NAME" $GIT_REPO ./src
+git clone --depth 1 --branch "$BRANCH" $GIT_REPO ./src
 rm -rf ./src/.git
 mv -f ./src/* ../
 rm -rf ./src
